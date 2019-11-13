1f3bac2122074f1724813c959e9b5993b100d1d0
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-11-13 18:28:49 +0900

    part5 quiz fetch

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import QuizModel from '../../models/Quiz';
 
 class Quiz extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentIndex: 0,
+            numberOfCorrected: 0,
+            quizzes: [],
+        };
+    }
+
+    async componentDidMount() {
+        await this.quizFetch();
+    }
+
+    async quizFetch() {
+        this.setState({
+            currentIndex: 0,
+            numberOfCorrected: 0,
+            quizzes: [],
+        });
+        const quizzes = await QuizModel.quizFetchAndCreateQuizInstances();
+        this.setState({ quizzes });
+    }
+
     render() {
+        let quizRender;
+        if (this.state.quizzes.length) {
+            quizRender = <h2>Quiz get.</h2>;
+        } else {
+            quizRender = <p>Now Loading...</p>
+        }
         return (
             <div>
                 <h1>Quiz</h1>
+                {quizRender}
+                <hr/>
                 <Link to="/">Homeへ</Link>
             </div>
         );
